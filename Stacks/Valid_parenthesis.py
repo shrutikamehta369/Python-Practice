2a85c751b755820865263c54531fd4637ad00cfe
Shrutika Mehta <shrutikamehta369@gmail.com>
2026-03-20 20:01:10 +0530

    Stack Questions

--- Stacks/Valid_parenthesis.py
+++ Stacks/Valid_parenthesis.py
@@ -1,3 +1,5 @@
+### use dictionary like line 6: clsoing braces should be the key as it will be easy to check for the opening braces when you see any opening brace add to stack and when you see any closing one validate it from the dictionary.
+
 class Solution:
     def isValid(self, s: str) -> bool:
         stack=[]
@@ -17,6 +19,9 @@ class Solution:
                     break
                 stack.pop()    
         return len(stack)==0
+    
+
+    
         
         
         
