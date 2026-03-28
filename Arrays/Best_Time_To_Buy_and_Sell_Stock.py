8c26c6ec8d1c8f829de644f000307cb1e63b11df
Shrutika Mehta <shrutikamehta369@gmail.com>
2026-03-28 22:14:40 +0530

    Array

---
+++ Arrays/Best_Time_To_Buy_and_Sell_Stock.py
@@ -0,0 +1,13 @@
+
+
+profit=max_profit=0
+arr=[7,1,5,3,6,4]
+min_val=arr[0]
+for i in arr:
+    if i < min_val:
+        min_val=i
+    else:
+        profit=i-min_val
+        if profit > max_profit:
+            max_profit=profit
+print (max_profit)    
